32547d6d21653e79c1be651ca324312f4f4e5c28
shinbeann <gayshinlee@gmail.com>
2024-08-07 23:58:03 +0800

    https://github.com/shinbeann/ESC-Scheduler.git

--- index.js
+++ index.js
@@ -14,6 +14,13 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 8002;
 
+
+// Middleware 
+app.use(cors({ 
+    origin: 'http://localhost:3000' 
+})); 
+app.use(express.json());
+
 // Routes
 app.get('/', (req, res) => {
     res.send('Express App containing API calls for Twilio, SMTP, and QR Code generation');
@@ -33,6 +40,3 @@ app.listen(PORT, async () => {
     }
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
